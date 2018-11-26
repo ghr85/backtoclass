d59f1d159d36fc199ad0859657f995d3cf505137
ghr85 <ghrenfrew@gmail.com>
2018-11-26 21:07:06 +0000

    updated some commentary

--- back_to_class.rb
+++ back_to_class.rb
@@ -4,16 +4,21 @@ this is handy for knowing how to create, store and recall variables.
 Keep this for playing around with.
 INTRO
 
+
+#defines the computer
+
+
 class Computer
-  @@users = {}
+  @@users = {} #builds an empty array
   def initialize(username,password)
     @username = username
     @password = password
     @files = {}
     @@users[username] = password
   end
+
   def create(filename)
-    time = Time.now
+    time = Time.now #generates a timestamp for current time
     @files[filename] = time
     puts "A new file #{filename},was created on #{time}."
   end
